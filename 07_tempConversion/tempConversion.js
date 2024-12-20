40f48d2608eb2fe958147e2536fd1a46ebe5d904
malapakavivek <malapakavivek628@gmail.com>
2024-12-20 14:03:12 +0530

    finished assignment 07;

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celcius = (fahrenheitTemp - 32)* (5/9)
+  if (celcius%10!=0){
+    return Number(celcius.toFixed(1));
+  }
+  return Number(celcius)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemp) {
+  let fahrenheit = (celciusTemp*(9/5)) + 32
+  if (fahrenheit%10!=0){
+    return Number(fahrenheit.toFixed(1));
+  }
+  return Number(fahrenheit)
 };
 
 // Do not edit below this line
