3bc8c0739ff578db153061a78a4799d8ca8a6441
malapakavivek <malapakavivek628@gmail.com>
2024-12-20 11:01:31 +0530

    added an additional method to reverse a string;

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,3 +3,12 @@ const reverseString = function(string) {
 };    
 // Do not edit below this line
 module.exports = reverseString;
+
+/*
+string = ""
+for (i=str.length-1;i>=0;i++){
+    string+=str[i]
+}
+    return string;
+
+*/
