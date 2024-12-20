ae86905c85fc5206fc1d86112cf7eca4758bd0d8
malapakavivek <malapakavivek628@gmail.com>
2024-12-20 10:41:32 +0530

    finished assignment 02;

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let string = "";
+    if (times<0){
+        return "ERROR";
+    }
+    if (times==0){
+        return "";
+    }
+    for (let i=0;i<times;i++){
+        string+=word;
+    }
+    return string;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
