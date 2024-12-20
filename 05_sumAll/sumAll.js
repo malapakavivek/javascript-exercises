8d452a3b42e9a308f52fb42a0f8caff455335778
malapakavivek <malapakavivek628@gmail.com>
2024-12-20 12:00:00 +0530

    finished assignment 05;

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(number1,number2) {
+    let answer = 0;
+
+    if (number1<0 || number1!=Number(number1)|| number1%1!=0){
+        return "ERROR"
+    }
+
+    if (number2<0 || number2!==Number(number2)|| number1%1!=0){
+        return "ERROR"
+    }
+
+    if (number1<number2) {
+        for (let i = number1; i<=number2;i++){
+            answer+=i
+        }
+        return answer
+    }
+    if (number1>number2) {
+        for (let i = number2; i<=number1;i++){
+            answer+=i
+        }
+        return answer
+    }
 
 };
 
